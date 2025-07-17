4d4709c6d174d07e0b7f2338d184c18178d3c2fa
BalajiLeninrajan <bleninra@uwaterloo.ca>
2025-07-17 16:47:44 -0400

    fix broken unmap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("v", "u")
+vim.keymap.set("v", "u", "<Nop>", { silent = true, noremap = true })
